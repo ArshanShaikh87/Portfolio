2a0827ab368b6d8d5c14f8fc66204c3f3dd9d65a
ArshanShaikh87 <viparsh4040@gmail.com>
2026-01-03 22:43:54 +0530

    Hero/Home section design completed

--- src/sections/home.jsx
+++ src/sections/home.jsx
@@ -1,23 +1,51 @@
+import { useEffect, useState } from 'react';
 import '../styles/home.css';
 
 function Home() {
+  const [showContent, setShowContent] = useState(false);
+
+  useEffect(() => {
+    // typing duration = 3s (same as CSS)
+    const timer = setTimeout(() => {
+      setShowContent(true);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <section id="home" className="home">
       <div className="home-content">
-        <h1>
-          Hi, I’m<span>Arshan</span>
+        <h1 className="typing">
+          Hi, I’m <br />
+          <span>Arshan Shaikh</span>
         </h1>
 
-        <h3>Full Stack Java Developer</h3>
+        {/* Content appears AFTER typing */}
+        <div className={`hero-reveal ${showContent ? 'show' : ''}`}>
+          <h3>Full Stack Developer</h3>
 
-        <p>
-          I build modern, responsive, and scalable web applications using
-          React, Spring Boot, and Oracle Database.
-        </p>
+          <p>
+            I build modern, responsive, and scalable web applications using
+            React, Spring Boot, and Oracle Database.
+          </p>
 
-        <div className="home-btns">
-          <a href="#portfolio" className="btn primary">View Portfolio</a>
-          <a href="#contact" className="btn">Contact Me</a>
+          <div className="home-btns">
+            <a href="#contact" className="btn">
+              Contact Me
+            </a>
+
+            <a
+            href="https://github.com/ArshanShaikh87"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn primary">
+            <i className="fa-brands fa-github"></i>
+            <span> Github</span>
+            </a>
+
+            
+          </div>
         </div>
       </div>
     </section>
