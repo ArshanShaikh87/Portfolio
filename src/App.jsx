a9c2db483a81a36254766da60f67df6d581ea601
ArshanShaikh87 <viparsh4040@gmail.com>
2026-01-04 10:49:13 +0530

    After resolivng git initial commit porblem first commit to check vercel working ok or not

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Services from './sections/services';
 import Portfolio from './sections/portfolio';
 import Contact from './sections/contact';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import Footer from './components/footer';
 
 function App() {
   return (
